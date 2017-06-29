c33939f6b3bd0b2b132745b759c60f04c32525e3
Pedro Ayesta <pedro_ayesta@es.ibm.com>
2017-06-29 12:42:33 +0000

    Simplificar el despliegue. Se hace tal cual y luego otro proceso cambiara todo a SSL

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,7 +31,6 @@ echo 'password' > password.txt
 tr -d '\n' <password.txt >.strippedpassword.txt && mv .strippedpassword.txt password.txt
 kubectl create -f local-volumes.yaml
 kubectl create secret generic mysql-pass --from-file=password.txt
-kubectl create configmap domain-config --from-literal=domain=$DOMAIN
 kubectl create -f mysql-deployment.yaml
 kubectl create -f wordpress-deployment.yaml
 kubectl create -f ftp-deployment.yaml
@@ -40,13 +39,9 @@ sleep 10s
 
 PORT=$(kubectl get service wordpress | grep wordpress | sed 's/.*://g' | sed 's/\/.*//g')
 POD=$(kubectl get pods | grep wordpress | sed 's/.*://g' | sed 's/\ .*//g')
-POD="$POD:/var/certs"
-mycrt="$DOMAIN.crt"
-mykey="$DOMAIN.key"
-ls -lal $mykey
-echo "$POD"
-kubectl cp $mycrt $POD
-kubectl cp $mykey $POD
+POD="$POD:/var/www/html/.user.ini"
+
+kubectl cp .user.ini $POD
 
 echo ""
 echo "View the wordpress at http://$IP_ADDR:$PORT"
